4fa1243494d296b1a6a377f61fbb917b6b39c220
bluecification <35982379+bluecification@users.noreply.github.com>
2019-06-25 23:23:12 -0500

    Update main.py
    Added basic YouTube video functionality. Next up: converting everything to embeds, (maybe) adding the functionality to react to a message to get lyrics so that it doesn't kill chat. Adding a queue command so that it doesn't get clogged, and also maybe an add/remove command to add/remove songs from the queue.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
 import discord
+from discord import opus
 from discord.ext import commands
-from discord.ext.commands import has_permissions
 import lyricsgenius
 import json
+import youtube_dl
+import asyncio
 
 prefix = "!"
+players = {}
+
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+        try:
+            opus.load_opus(opus_lib)
+            return
+        except OSError:
+            pass
+
+        raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
 
 ##If you're going to try running this, make sure you create tokens.json with a genius_login_token and discord_bot_token for this to work
 with open('hackweek/tokens.json') as f:
@@ -23,12 +41,12 @@ async def on_ready():
 #async def on_message(message):
 #    await bot.process_commands(message)
 
-@bot.command()
+@bot.command(pass_context=True)
 async def ping(ctx):
     await ctx.send("Response Time: " + str(round(bot.latency,2)) + " seconds")
 
-@bot.command()
-async def searchsong(ctx, song_name, song_author):
+@bot.command(pass_context=True)
+async def lyrics(ctx, song_name, song_author):
     #artist = genius.search_artist(song_author, max_songs=1, sort="title")
     song = genius.search_song(song_name, song_author)
     lyrics = song.lyrics
@@ -39,12 +57,74 @@ async def searchsong(ctx, song_name, song_author):
         await ctx.send(lyrics[i*2000: (i+1)*2000])
         i+=1
 
-
-@searchsong.error
+@lyrics.error
 async def clear_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("You're missing something! Do {}help {} to see what you're missing!".format(prefix, ctx.command))
     
     raise error
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+bot.voiceclient = None
+
+@bot.command(pass_context=True)
+async def play(ctx, *, url):
+    
+        voice_channel = ctx.message.author.voice.channel
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=bot.loop)
+            bot.voiceclient = await voice_channel.connect(reconnect=True)
+            bot.voiceclient.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        await ctx.send('Now playing: {}'.format(player.title))
+
+@bot.command(pass_context=True)
+async def stop(ctx):
+    await bot.voiceclient.disconnect()
+    await ctx.send("Miscord has successfully disconnected from your voice channel!")
+    bot.voiceclient = None
 
 bot.run(tokensjson["discord_bot_token"])
