eed6f6cc380a391cfa1367733e674481207feb9c
bluecification <35982379+bluecification@users.noreply.github.com>
2019-06-27 17:42:32 -0500

    Update main.py
    Fixed everything.

--- src/main.py
+++ src/main.py
@@ -107,7 +107,7 @@ async def delrole(ctx, *,role_name):
       await ctx.send(":warning: I am missing permissions to delete this role!")
   else:
     await ctx.send("That role doesn't exist!")
-
+    
 @bot.command(pass_context=True)
 async def ping(ctx):
     em = discord.Embed(title="Response Time: " + str(round(bot.latency,2) * 1000) + " ms", description="", color= 0x2a988d)
@@ -147,32 +147,22 @@ async def lyrics(ctx, song_name, song_author):
             await ctx.send(lyrics[i*2000: (i+1)*2000])
             i+=1
 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download= stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url']
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
 bot.voiceclient = None
 
 @bot.command(pass_context=True)
 async def play(ctx, *, url):
-
+    
     voice_channel = ctx.message.author.voice.channel
 
     async with ctx.typing():
         player = await YTDLSource.from_url(url, loop=bot.loop)
         bot.voiceclient = await voice_channel.connect(reconnect=True)
         bot.voiceclient.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
+        
+    bot.queue_titles.append(player.title)    
     bot.queue_list.append(url)
     em = discord.Embed(title="Miscord is now playing:", description="{}".format(player.title), color= 0x2a988d)
+    em.set_footer(text="Utile Team 2019")
     await ctx.send(embed=em)
     bot.joinvoice_text_channel = ctx.message.channel
 
@@ -183,13 +173,15 @@ async def update_queue():
             if bot.voiceclient_pause == False:
 
                 del bot.queue_list[0]
+                del bot.queue_titles[0]
 
                 if (len(bot.queue_list) == 0):
                     await bot.voiceclient.disconnect()
                     bot.voiceclient = None
                     em = discord.Embed(title="No videos left in queue. Miscord has left the voice channel.", color= 0x2a988d)
+                    em.set_footer(text="Utile Team 2019")
                     await bot.joinvoice_text_channel.send(embed=em)
-
+        
                 else:
                     player = await YTDLSource.from_url(bot.queue_list[0], loop=bot.loop)
                     bot.voiceclient.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
@@ -204,26 +196,45 @@ async def stop(ctx):
     bot.queue_list = []
 
 @bot.command(pass_context=True)
+async def stop(ctx):
+    await bot.voiceclient.disconnect()
+    
+    em = discord.Embed(title="Miscord has succesfully disconnected!", color=0x2a988d)
+    em.set_footer(text="Utile Team 2019")
+    await ctx.send(embed=em)
+    bot.voiceclient = None
+    bot.queue_list = []
+
+@bot.command(pass_context=True)
 async def queue(ctx):
     em = discord.Embed(title="Queue: ", color=0x2a988d)
-    for index, song in enumerate(bot.queue_list):
-        em.add_field(name="Number {}".format(str(index+1)), value=song)
 
+    for index, song in enumerate(bot.queue_list):
+        em.add_field(name="{}".format(bot.queue_titles[index]), value=song)
+    
+    em.set_footer(text="Utile Team 2019")
     await ctx.send(embed=em)
 
 @bot.command(pass_context=True)
 async def add(ctx, *, url):
+    player = await YTDLSource.from_url(url, loop=bot.loop)
     bot.queue_list.append(url)
+    bot.queue_titles.append(player.title)
+
     em = discord.Embed(title="Adding to Queue: ", color=0x2a988d)
-    em.add_field(name="URL: ", value="{}".format(url))
+    em.add_field(name="{}".format(player.title), value="{}".format(url))
+    em.set_footer(text="Utile Team 2019")
     await ctx.send(embed=em)
 
 @bot.command(pass_context=True)
 async def remove(ctx, *, number:int):
     deleted_url = bot.queue_list[number-1]
+    deleted_title = bot.queue_titles[number-1]
     del bot.queue_list[number-1]
+    del bot.queue_titles[number-1]
     em = discord.Embed(title="Removing from Queue: ", color=0x2a988d)
-    em.add_field(name="URL: ", value="{}".format(deleted_url))
+    em.add_field(name="{}".format(deleted_title), value="{}".format(deleted_url))
+    em.set_footer(text="Utile Team 2019")
     await ctx.send(embed=em)
 
 @bot.command(pass_context=True)
@@ -242,8 +253,15 @@ async def resume(ctx):
         bot.voiceclient_pause = False
     else:
         em = discord.Embed(title="Miscord isn't in a voice channel!", color=0x2a988d)
+        em.set_footer(text="Utile Team 2019")
         await ctx.send(embed=em)
 
+@bot.command(pass_context=True)
+async def skip(ctx):
+    em = discord.Embed(title= "{} has been skipped!".format(bot.queue_list[0]), color=0x2a988d)
+    bot.voiceclient.stop()
+    await ctx.send(embed=em)	
+	
 @bot.command()
 async def feedback(ctx, feedback_message):
     await ctx.message.delete() #removes the message to make the server cleaner
@@ -254,29 +272,13 @@ async def feedback(ctx, feedback_message):
     await feedbackChannel.send("<@&593916238908883002> Message from @{}: {}.".format(ctx.message.author, feedback_message)) #sends the feedback to the feedback channel
     await sender.send(":incoming_envelope: Your feedback has been sent! Thanks for helping us making **Miscord** a better bot.")
     await sender.send("An admin has been notified and will contact you throughout the day, meanwhile, you can join the **Support Server** if you want to continue the discussion.")
-
+	
 @bot.command(pass_context = True)
 async def mute(ctx, member: discord.Member):
     role = discord.utils.get(member.server.roles, name='Muted')
-    await bot.add_roles(member, role)
+    await member.add_roles(role)
     embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
-    await bot.send(embed=embed)
-
-@bot.command()
-async def searchsong(ctx, song_name, song_author):
-    #artist = genius.search_artist(song_author, max_songs=1, sort="title")
-    song = genius.search_song(song_name, song_author)
-    if song != None:
-        lyrics = song.lyrics
-        messagelength = int(len(lyrics)/2000)
-
-        i = 0
-        while i < (messagelength + 1):
-            await ctx.send(lyrics[i*2000: (i+1)*2000])
-            i+=1
-    else:
-        await ctx.send("Woops, it seems I can't find **{}** by **{}** anywhere...".format(song_name, song_author))
-        await ctx.send("Do you think this might be our problem? Report it using the `!feedback` command.")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def kick(ctx, member: discord.Member, reason: str='No reason supplied'):
@@ -314,7 +316,7 @@ async def on_command_error(ctx, error):
 
 @kick.error
 async def clear_error(ctx, error):
-    if isinstance(error, commands.CheckFailure): #returns true when the user doesn't have permissions
+    if isinstance(error, CheckFailure): #returns true when the user doesn't have permissions
         errMessage = await client.send(ctx.message.channel, "Looks like you don't have the required permissions to kick users.")
         time.sleep(5)
         await ctx.message.delete()
@@ -322,7 +324,7 @@ async def clear_error(ctx, error):
 
 @ban.error
 async def clear_error(ctx, error):
-    if isinstance(error, commands.CheckFailure): #returns true when the user doesn't have permissions
+    if isinstance(error, CheckFailure): #returns true when the user doesn't have permissions
         errMessage = await client.send(ctx.message.channel, "Looks like you don't have the required permissions to ban users.")
         time.sleep(5)
         await ctx.message.delete()
@@ -373,6 +375,19 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
         self.title = data.get('title')
         self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download= stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url']
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
 
 # __________________________________________________________________________________________________________________________________________#
 "Run bot"
