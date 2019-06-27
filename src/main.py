ee9daad6b57ba2aab3e6ca397a63fc3a0d724d85
SemiiColon <37005919+SemiiColon@users.noreply.github.com>
2019-06-27 15:49:48 -0700

    Added m!magic8ball command.

--- src/main.py
+++ src/main.py
@@ -92,6 +92,18 @@ async def createRole(ctx, role):
     await guild.create_role(name="{}".format(role))
     await ctx.send("Created role {} successfully!".format(role))
 
+  @bot.command(pass_context=true)
+  async def magic8ball(ctx):
+    await bot.send(random.choice([
+      "It is certain. :8ball:", "It is decidedly so. :8ball:",
+      "Without a doubt. :8ball:", "Yes - definitely. :8ball:", "You may rely on it. :8ball:",
+      "As I see it, yes. :8ball:", "Most likely. :8ball:", "Outlook good. :8ball:",
+      "Yes. :8ball:", "Signs point to yes. :8ball:", "Reply hazy, try again. :8ball:",
+      "Ask again later. :8ball:", "Better not tell you now. :8ball:", "Cannot predict now. :8ball:",
+      "Concentrate and ask again. :8ball:", "Don't count on it. :8ball:", "My reply is no. :8ball:",
+      "My sources say no. :8ball:", "Outlook not so good. :8ball:", "Very doubtful. :8ball:"
+    ]))
+
 @bot.command(pass_context=True)
 async def assignRole(ctx, role_name, member: discord.Member):
     role = discord.utils.get(ctx.guild.roles, name="{}".format(role_name))
